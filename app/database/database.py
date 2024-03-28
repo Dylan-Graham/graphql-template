ffe9210f203bd07ce7775786334df4a813266985
Dylan Graham <dylan@explore-ai.net>
2024-03-28 15:58:14 +0100

    database connection

--- app/database/database.py
+++ app/database/database.py
@@ -1,7 +1,22 @@
-from typing import List
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from app.models.user import User
+# Define SQLAlchemy base
+Base = declarative_base()
 
 
-# "Database"
-Users: List[User] = [User(id=1, name="Patrick", age=100, gender="Male")]
+# Define SQLAlchemy User model
+class DBUser(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String)
+    age = Column(Integer)
+    gender = Column(String)
+
+
+# Set up SQLAlchemy engine and session
+engine = create_engine("sqlite:///app/database/user.sqlite")
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
--- app/resolvers/user_query.py
+++ app/resolvers/user_query.py
@@ -1,13 +1,30 @@
+from sqlalchemy import select
 import strawberry
 
-from app.models.user import User, Users
+from app.models.user import User
+from app.database.database import DBUser, Session
+
+
+# @strawberry.type
+# class Query:
+#     @strawberry.field
+#     def user(self, id: int) -> User:
+#         for user in Users:
+#             if user.id == id:
+#                 return user
+#         raise ValueError(f"User with id {id} not found")
 
 
 @strawberry.type
 class Query:
     @strawberry.field
     def user(self, id: int) -> User:
-        for user in Users:
-            if user.id == id:
-                return user
-        raise ValueError(f"User with id {id} not found")
+        session = Session()
+        db_user = session.query(DBUser).filter(DBUser.id == id).first()
+        session.close()
+        if db_user:
+            return User(
+                id=db_user.id, name=db_user.name, age=db_user.age, gender=db_user.gender
+            )
+        else:
+            raise ValueError(f"User with id {id} not found")
